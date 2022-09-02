b26546d8a575b6bf0cdd687c3c34c85e949b0186
User <user@kp11.ru>
2022-09-02 13:11:33 +0300

    Initial Commit

---
+++ firstapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstapp
+//
+//  Created by user on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
